e912eb6d45ceb7f2f72b9e730d9bbda2d66c3dc9
Biplab097 <biplabbarman097@gmail.com>
2020-03-09 13:09:14 +0530

    Adding image capturing class and methods

---
+++ imageScanAndStore.py
@@ -0,0 +1,19 @@
+import cv2
+
+class ImageCapturing:
+    def captureImage(self):
+        vidcap = cv2.VideoCapture(0)
+        count = 0
+        success = True
+        fps = int(vidcap.get(cv2.CAP_PROP_FPS))
+
+        while count<500:
+            success, image = vidcap.read()
+            print('read a new frame:', success)
+            if count % (10 * fps) == 0:
+                cv2.imwrite('frame%d.jpg' % count, image)
+                print('successfully written 10th frame')
+            count += 1
+
+ob = ImageCapturing()
+ob.captureImage()
