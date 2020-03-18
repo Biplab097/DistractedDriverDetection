8811965bb14f0be59fd630668baa1ad8cb02394c
Biplab097 <biplabbarman097@gmail.com>
2020-03-18 11:27:14 +0530

    Adding image capturing class and methods to store images at particular interval

--- imageScanAndStore.py
+++ imageScanAndStore.py
@@ -7,13 +7,14 @@ class ImageCapturing:
         success = True
         fps = int(vidcap.get(cv2.CAP_PROP_FPS))
 
-        while count<900:
+        while count<700:
             success, image = vidcap.read()
             print('read a new frame:', success)
             if count % (10 * fps) == 0:
                 cv2.imwrite('frame%d.jpg' % count, image)
                 print('successfully written 10th frame')
             count += 1
+
 
 ob = ImageCapturing()
 ob.captureImage()
